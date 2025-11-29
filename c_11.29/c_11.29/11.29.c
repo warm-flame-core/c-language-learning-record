5d1ec2cda64d1b02ee073aa727b4cb6bdecd5641
warm-flame-core <1994768512@qq.com>
2025-11-29 20:22:59 +0800

    C语言学习第一天

---
+++ c_11.29/c_11.29/11.29.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>  //创建头文件，引入函数
+
+int main()  //使用main函数，每个项目都有且只有一个main函数作为程序入口
+{
+	printf("hello world\n");  //打印内容，并换行
+	getchar();
+	return 0;
+}
