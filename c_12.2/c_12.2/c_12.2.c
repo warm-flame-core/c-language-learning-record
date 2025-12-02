1c59c9cc1f4c6daa11b5c8a24c4d3584715f43cb
warm-flame-core <1994768512@qq.com>
2025-12-02 16:01:18 +0800

    学习第2天，字符长度，注释，变量，sizeof，signed和unsigned区别

---
+++ c_12.2/c_12.2/c_12.2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<string.h>
+#include<stdio.h>
+//int main()
+//{
+//	int len = strlen("basdkjahslfe");
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+int main()
+{
+	int age = 0;//≥ı ºªØ
+
+	float score;
+	score = 20.0f;//∏≥÷µ
+	printf("%lf\n", score);
+	return 0;
+}
