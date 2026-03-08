0cdfaf547935da5e9d2036504d30e5ff6971fc0f
warm-flame-core <1994768512@qq.com>
2026-03-08 20:47:42 +0800

    更新大小端学习

--- c_study_2026/c_study_2026/Memory_function.c
+++ c_study_2026/c_study_2026/Memory_function.c
@@ -59,35 +59,56 @@
 
 
 //memmove函数模拟实现
-void* my_memmove(void* dst, void* src, size_t num)
-{
-	void *ret = dst;
-	//目标位置在原位置前面，从前往后
-	if (dst < src)
-	{
-		while (num--)
-		{
-		*(char*)dst = *(char*)src;
-		dst = (char*)dst + 1;
-		src = (char*)src + 1;
-		}
-	}
-	//目标位置在原位置后面，从后往前
-	else
-		while (num--)
-		{
-			*((char*)dst + num) = *((char*)src + num);
-		}
-	return ret;
-}
+//void* my_memmove(void* dst, void* src, size_t num)
+//{
+//	void *ret = dst;
+//	//目标位置在原位置前面，从前往后
+//	if (dst < src)
+//	{
+//		while (num--)
+//		{
+//		*(char*)dst = *(char*)src;
+//		dst = (char*)dst + 1;
+//		src = (char*)src + 1;
+//		}
+//	}
+//	//目标位置在原位置后面，从后往前
+//	else
+//		while (num--)
+//		{
+//			*((char*)dst + num) = *((char*)src + num);
+//		}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,0 };
+//	my_memmove(arr1, arr1+2, 20);
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	return 0;
+//}
+
+
+//memset函数
+//int main()
+//{
+//	char arr[] = "Hello World";
+//	memset(arr, 'x', 5);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//memcmp函数
 int main()
 {
-	int arr1[] = { 1,2,3,4,5,6,7,8,9,0 };
-	my_memmove(arr1, arr1+2, 20);
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", arr1[i]);
-	}
+	char arr1[] = "Hello";
+	char arr2[] = "hello";
+	int r = memcmp(arr1, arr2, 2);
+	printf("%d\n", r);
 	return 0;
 }
---
+++ c_study_2026/c_study_2026/data_storage.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int check_sys()
+{
+	int i = 1;
+	return *(char*)&i;
+}
+int main()
+{
+	if (check_sys)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	return 0;
+}
