b0f995d1e6e221e9e175b327304d4f268e7e7ea0
warm-flame-core <1994768512@qq.com>
2026-03-08 21:08:45 +0800

    更新学习

--- c_study_2026/c_study_2026/data_storage.c
+++ c_study_2026/c_study_2026/data_storage.c
@@ -1,19 +1,27 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-int check_sys()
-{
-	int i = 1;
-	return *(char*)&i;
-}
+//#include <stdio.h>
+//int check_sys()
+//{
+//	int i = 1;
+//	return *(char*)&i;
+//}
+//int main()
+//{
+//	if (check_sys)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
+
+
+//观察内存中的排序
 int main()
 {
-	if (check_sys)
-	{
-		printf("小端\n");
-	}
-	else
-	{
-		printf("大端\n");
-	}
+	int a = 0x11223344;
 	return 0;
 }
