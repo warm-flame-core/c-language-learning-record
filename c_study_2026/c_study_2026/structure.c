407edffc2edeef4cff9c41691d8277ba8498eb90
warm-flame-core <1994768512@qq.com>
2026-03-10 21:51:57 +0800

    更新联合体和枚举的学习

---
+++ c_study_2026/c_study_2026/structure.c
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//struct stduent
+//{
+//	char name[20];
+//	int age;
+//	int num;
+//};
+//int main()
+//{
+//	struct stduent s = { .name = "wangwu",.num = 20,.age = 18 };
+//	printf("%s\n", s.name);
+//	return 0;
+//}
+
+
+//结构体指针
+//struct stduent
+//{
+//	char name[20];
+//	int age;
+//	int num;
+//};
+//struct stduent s = { .name = "王五",.num = 20,.age = 18 };
+//void print1(struct stduent* s)
+//{
+//	printf("%s\n", s->name);
+//}
+//int main()
+//{
+//	print1(&s);
+//	return 0;
+//}
+
+
+//结构体数组
+//struct stu
+//{
+//	char arr[20];
+//	int age;
+//};
+//struct stu arr[10] = { 0 };
+
+
+//结构体对齐数
+//#pragma pack(1)
+//int main()
+//{
+//
+//	return 0;
+//}
+
+
+//位段:不能取地址操作，用scanf获取应该先给其他变量再赋值，因为位段的成员可能在同一地址
+struct S
+{
+	char a : 2;
+	char b : 3;
+	char c : 4;
+};
---
+++ c_study_2026/c_study_2026/union_enum.c
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//联合体大小
+//union stu 
+//{
+//	char c;
+//	int i;
+//};
+//int main()
+//{
+//	union stu un = { 0 };
+//	printf("%zu\n", sizeof(un));
+//}
+
+
+//联合体特点
+//union stu
+//{
+//	char c;
+//	int i;
+//};
+//int main()
+//{
+//	union stu un = { 0 };
+//	un.i = 0x11223344;
+//	un.c = 0x55;
+//	printf("%x\n", un.i);
+//}
+
+
+//用联合体判断大小字节序
+//union stu
+//{
+//	char c;
+//	int i;
+//};
+//int main()
+//{
+//	union stu un = { 0 };
+//	un.i = 1;
+//	if (un.c)
+//	{
+//		printf("小段\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//}
+
+
+//枚举
+enum Sex
+{
+	MALE=5,
+	FEMALE,
+	SECRET
+};
+
+enum Sex clr = FEMALE;//在C语言中可以赋值，在C++中不可以
