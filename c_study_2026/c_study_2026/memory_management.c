e8ec7f958fed905ba39658107328b7dd46522a69
warm-flame-core <1994768512@qq.com>
2026-03-12 17:35:42 +0800

    更新动态内存管理

---
+++ c_study_2026/c_study_2026/memory_management.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	//malloc函数申请内存空间
+	int* p = (int*)malloc(10 * sizeof(int));
+	
+	int i = 0;
+	if (p != NULL)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			*(p + i) = i + 1;
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	//free函数释放内存空间
+	free(p);
+	return 0;
+}
